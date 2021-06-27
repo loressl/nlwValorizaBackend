adf20cb74f35fcba83e61e721d5e83cc402fa1d8
loressl <loryssl@hotmail.com>
2021-06-27 00:27:35 -0300

    Landing.

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/sessions", authenticateUserController.handle);
 
 export { router };
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { sign } from "jsonwebtoken";
+import { config } from "dotenv";
+config();
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
